6a7442632ed1245a9bcda51b44a25daf4917830f
Hwayoung Yoon <ghkdud204@gmail.com>
2021-10-11 13:33:47 +0900

    add shiny tutorial: hello shiny

---
+++ shiny-test/tutorial/shiny 아키텍처 이해.R
@@ -0,0 +1,50 @@
+##### Hello Shiny! ----
+
+install.packages("shiny")
+devtools::install_github("rstudio/shinytest", ref = "fc80bdbfe68bcd00329d61ef0cea8bc3cee00929")
+
+library(shiny)
+runExample("01_hello")
+
+
+##### 히스토그램 그리기 예저 : UI&Server 상호작용 확인 ----
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
